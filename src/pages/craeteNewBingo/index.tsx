7e93509af82646935f8bc5d78e9ae0356512b6cc
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-11-28 13:31:49 +0300

    Закончил создание и публикация бинго

--- src/pages/craeteNewBingo/index.tsx
+++ src/pages/craeteNewBingo/index.tsx
@@ -1,4 +1,13 @@
-import { useRef, useState, FormEvent, FunctionComponent } from "react"
+import axios from "axios"
+import { useRef, useState, FormEvent, FunctionComponent, createContext, useEffect, useContext, memo } from "react"
+
+
+interface ContextType {
+    editCard: (text: string, id: number) => void
+    removeCard: (id: number) => void
+}
+
+const CardsContext = createContext<ContextType>({} as ContextType)
 
 
 
@@ -7,8 +16,6 @@ interface CardType {
     text: string
 }
 
-
-
 function CreateNewBingo() {
     const [bingos, setBingos] = useState<CardType[]>([])
     const inputRef = useRef<HTMLInputElement>(null)
@@ -16,6 +23,7 @@ function CreateNewBingo() {
     function addCard(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
         if (!inputRef.current) return
+        if (inputRef.current.value.length <= 0) return
         const currentId = bingos.length
         const newCard: CardType = {
             id: currentId,
@@ -32,34 +40,84 @@ function CreateNewBingo() {
         })
     }
 
+    function editCard(text: string, id: number) {
+        setBingos(() => bingos.map(item => item.id === id ? { ...item, text } : item))
+    }
+
+    function saveBingo() {
+        axios({
+            url: `${process.env.REACT_APP_BACKEND_URL}/create`,
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            data: JSON.stringify({ template: bingos.map(item => item.text) })
+        }).then(console.log)
+    }
+
+    const context = {
+        editCard,
+        removeCard
+    }
+
     return (
         <div>
-            <form onSubmit={addCard}>
-                <input ref={inputRef} />
-                <button type='submit'> Добавить новое поле</button>
-            </form>
+            <CardsContext.Provider value={context}>
+                <form onSubmit={addCard}>
+                    <input ref={inputRef} />
+                    <button type='submit'> Добавить новое поле</button>
+                </form>
+                <div>
+                    {bingos.map((item) => <SingleBingoCard key={item.id} id={item.id} text={item.text} />)}
+                </div>
+            </CardsContext.Provider>
+            <button onClick={() => { saveBingo() }}>Сохранить бинго!</button>
+        </div>
+    )
+}
+
+const SingleBingoCard: FunctionComponent<CardType> = memo(({ text, id }) => {
+    const [edit, setEdit] = useState<boolean>(false)
+
+    const changeMode = () => setEdit(prev => !prev)
+
+
+    return edit ?
+        <EditCard text={text} id={id} changeMode={changeMode} /> : (
             <div>
-                {bingos.map((item) => <SingleBingoCard key={item.id} id={item.id} text={item.text} removeCard={removeCard} />)}
+                <span>{text}</span>
+                <button onClick={() => setEdit(prev => !prev)}>
+                    Редактировать
+                </button>
+                <button>Удалить</button>
             </div>
-        </div>
-    )
+        )
+})
+
+
+interface EditProps extends CardType {
+    changeMode: () => void
 }
 
-interface BingoCardProps {
-    id: number
-    text: string
-    removeCard: (id: number) => void
-}
+const EditCard: FunctionComponent<EditProps> = memo(({ id, text, changeMode }) => {
+    const { editCard } = useContext(CardsContext)
+    const input = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+        if (!input.current) return
+        input.current.value = text
+    }, [text])
 
-const SingleBingoCard: FunctionComponent<BingoCardProps> = ({ text, removeCard, id }) => {
     return (
         <div>
-            <span>{text}</span>
+            <input ref={input}></input>
             <button onClick={() => {
-                removeCard(id)
-            }}>Удалить</button>
+                if (!input.current) return;
+                editCard(input.current.value, id)
+                changeMode()
+            }}>Сохранить</button>
+            <button onClick={() => changeMode()}>Отменить</button>
         </div>
     )
-}
+})
 
 export default CreateNewBingo
